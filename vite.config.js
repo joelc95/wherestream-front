d3166d66cf911b57c15568808b9bf17273ca9466
Joel Conalty <joelconalty@gmail.com>
2023-10-31 21:19:52 +0000

    homepage added to package

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/wherestream-front/",
   plugins: [react()],
 	define: {
 		'process.env': {}
 	},
-	base: "/wherestream-front/"
 })
