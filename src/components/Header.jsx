ce06f855f5f41f2cc932bba1692ecf3453c5f240
Joel Conalty <joelconalty@gmail.com>
2023-10-31 22:07:43 +0000

    href in heade logo changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Logo from '../logo-transparent.svg'
 export default function Header() {
 	return (
 		<div className='flex justify-between p-5'>
-			<span ><a className='flex flew-row gap-1' href="/"><h1 className="text-3xl font-bold font-mono text-green-300">wherestream</h1>
+			<span ><a className='flex flew-row gap-1' href="."><h1 className="text-3xl font-bold font-mono text-green-300">wherestream</h1>
 			<img src={Logo} alt="logo" className='h-10'/></a></span>
 		</div>
 	)
